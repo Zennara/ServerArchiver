4bb82ad75ecc2a5b8f8e217b0041ede12539b4c9
Keagan Landfried <64995253+Zennara@users.noreply.github.com>
2023-05-20 16:36:37 -0700

    archive command

--- main.py
+++ main.py
@@ -11,13 +11,35 @@ config = json.load(f)  # load file into dict
 f.close()  # close file
 
 guild_ids = config["guild_ids"]  # comma separated guild ids
+
+
 @bot.event
 async def on_ready():
     print(f"{bot.user.name} Online.")
 
+
 @bot.slash_command(description="Archive the current server", guild_ids=guild_ids)
-async def copy(ctx):
-    pass
+async def archive(ctx):
+    # thinking message
+    await ctx.defer()
+
+    # loop through server categories
+    for category in ctx.guild.categories:
+        # loop through channels in category
+        for channel in category.channels:
+            # add archived permissions
+            await channel.set_permissions(ctx.guild.default_role,
+                                          send_messages=False,
+                                          add_reactions=False,
+                                          send_messages_in_threads=False,
+                                          create_public_threads=False,
+                                          create_private_threads=False,
+                                          connect=False,
+                                          create_instant_invite=False,
+                                          use_application_commands=False)
+
+    # confirmation message
+    await ctx.followup.send(content="Server successfully archived.")
 
 
 # run bot
